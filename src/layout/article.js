c613b1fff8c01bc586e9e88960baafd7505480c4
jhn2762 <jhn2762@gmail.com>
2023-01-05 17:58:44 +0900

    2023-01-05 17:58

--- src/layout/article.js
+++ src/layout/article.js
@@ -1,11 +1,39 @@
-import "./article.css";
+import './article.css';
+import React, { useState } from 'react';
 
-const Article = () => {
+function Article() {
+  const [inputs, setInputs] = useState({
+    name: '',
+    age: '',
+  });
+
+  const { name, age } = inputs;
+
+  // const [text, setText] = useState('');
+  const onChange = (e) => {
+    // setText(e.target.value);
+    const { name, value } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const onReset = () => {
+    // setText('');
+  };
+
   return (
     <>
-      <article>안녕하세요</article>
+      <article>
+        <h2>input</h2>
+        <input onChange={onChange} value={text}></input>
+        <button onClick={onReset}>초기화</button>
+        <input type="text"></input>
+        <input type="text"></input>
+      </article>
     </>
   );
-};
+}
 
 export default Article;
